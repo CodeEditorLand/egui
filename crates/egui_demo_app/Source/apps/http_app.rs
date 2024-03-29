e34778c34c6c93a8c3f4bf49a109ecfd544c5cbe
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-03-29 08:09:06 +0200

    squash!

--- crates/egui_demo_app/Source/apps/http_app.rs
+++ crates/egui_demo_app/Source/apps/http_app.rs
@@ -47,7 +47,7 @@ pub struct HttpApp {
 impl Default for HttpApp {
     fn default() -> Self {
         Self {
-            url: "https://Raw.GitHubUserContent.Com/emilk/egui/master/README.md".to_owned(),
+            url: "https://raw.githubusercontent.com/emilk/egui/master/README.md".to_owned(),
             promise: Default::default(),
         }
     }
@@ -125,7 +125,7 @@ fn ui_url(ui: &mut egui::Ui, frame: &mut eframe::Frame, url: &mut String) -> boo
     ui.horizontal(|ui| {
         if ui.button("Source code for this example").clicked() {
             *url = format!(
-                "https://Raw.GitHubUserContent.Com/emilk/egui/master/{}",
+                "https://raw.githubusercontent.com/emilk/egui/master/{}",
                 file!()
             );
             trigger_fetch = true;
